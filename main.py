477cd48efbdab2e0f649ae37bf130fed2f8f0fe7
Andrew Tran <atran1993@gmail.com>
2017-12-07 18:06:12 -0600

    Adding same details from main.ipynb into main.py

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ import numpy as np
 import keras as ks
 import matplotlib.pyplot as plt
 
+import scipy.signal as signal
+from scipy.io import wavfile
 
+#do FFT because indecies will be the same length of the file ??
+
+filepath = "./train/audio"
+filename = "/bed/00f0204f_nohash_0.wav"
+
+sample_rate, sample = wavfile.read(filepath + filename)
 
