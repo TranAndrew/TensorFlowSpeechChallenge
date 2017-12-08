8bf10fe95182af7bfac67548ad5d29c6516478e3
Andrew Tran <atran1993@gmail.com>
2017-12-07 18:16:58 -0600

    Completed graph from Kaggle information

--- main.py
+++ main.py
@@ -6,10 +6,48 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 from scipy.io import wavfile
 
+
+# Visualization copy pasted
+import matplotlib.pyplot as plt
+import seaborn as sns
+import IPython.display as ipd
+
 #do FFT because indecies will be the same length of the file ??
 
 filepath = "./train/audio"
 filename = "/bed/00f0204f_nohash_0.wav"
 
-sample_rate, sample = wavfile.read(filepath + filename)
+sample_rate, samples = wavfile.read(filepath + filename)
 
+
+# A bunch of copy paste stuff from
+# https://www.kaggle.com/davids1992/speech-representation-and-data-exploration
+
+def log_specgram(audio, sample_rate, window_size=20,
+                 step_size=10, eps=1e-10):
+    nperseg = int(round(window_size * sample_rate / 1e3))
+    noverlap = int(round(step_size * sample_rate / 1e3))
+    freqs, times, spec = signal.spectrogram(audio,
+                                    fs=sample_rate,
+                                    window='hann',
+                                    nperseg=nperseg,
+                                    noverlap=noverlap,
+                                    detrend=False)
+    return freqs, times, np.log(spec.T.astype(np.float32) + eps)
+
+freqs, times, spectrogram = log_specgram(samples, sample_rate)
+
+fig = plt.figure(figsize=(14, 8))
+ax1 = fig.add_subplot(211)
+ax1.set_title('Raw wave of ' + filename)
+ax1.set_ylabel('Amplitude')
+ax1.plot(np.linspace(0, sample_rate/len(samples), sample_rate), samples)
+
+ax2 = fig.add_subplot(212)
+ax2.imshow(spectrogram.T, aspect='auto', origin='lower',
+           extent=[times.min(), times.max(), freqs.min(), freqs.max()])
+ax2.set_yticks(freqs[::16])
+ax2.set_xticks(times[::16])
+ax2.set_title('Spectrogram of ' + filename)
+ax2.set_ylabel('Freqs in Hz')
+ax2.set_xlabel('Seconds')
